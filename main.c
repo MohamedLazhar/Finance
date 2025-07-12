cf8de98291bc302cc3a0b399d290b66faee16c48
MohamedLazhar <mohamedlazhardeghdak@gmail.com>
2025-07-13 00:55:47 +0200

    Update main.c
    Ajout  Main

--- main.c
+++ main.c
@@ -3,12 +3,20 @@
 // STRUCTURE DES DONNEES
 // Le Compte 
 
-struct Comptes
+typedef struct Comptes
 {
   int ID;
   Char Libelle[31];
   
 } Comptes;
 
+int main ()
+{
+  Comptes Compte[11];
+  scan("%d",Compte.ID[i]);
+  printf(" Les Comptes ID : %d Libelle : %c /n ", Compte.ID,Compte.Libelle);
+  
+  
+}
 
 
